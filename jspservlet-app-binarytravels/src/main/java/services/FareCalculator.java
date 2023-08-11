20b23260878db4225ceedd50da01b61fcde70eba
Iamneo <techadmin@iamneo.ai>
2023-08-11 07:03:45 +0000

    Backup Commit

--- jspservlet-app-binarytravels/src/main/java/services/FareCalculator.java
+++ jspservlet-app-binarytravels/src/main/java/services/FareCalculator.java
@@ -31,30 +31,11 @@ public class FareCalculator extends Booking{
 
     public double book(Hotel hotel) {
         double totalFare = 0;
-        
-        // Check if Check-In date is before Check-Out date
-        if (hotel.getFromdate().isBefore(hotel.getTodate())) {
-            int noOfPersons = hotel.getNoOfPersons();
-            String occupancy = hotel.getOccupancy();
-            int rates = hotel.getRates();
-    
-            // Calculate total fare based on occupancy
-            if (occupancy.equalsIgnoreCase("single") && noOfPersons == 1) {
-                totalFare = booking(noOfPersons, rates);
-            } else if (occupancy.equalsIgnoreCase("double") && noOfPersons == 2) {
-                totalFare = booking(noOfPersons, rates);
-            } else if (occupancy.equalsIgnoreCase("mixed") && noOfPersons == 3) {
-                // 1 Double Occupancy room and 1 Single Occupancy room
-                totalFare = 2 * booking(1, rates) + booking(1, rates);
-            } else {
-                // Invalid occupancy or number of persons
-                totalFare = 0; // Or set an appropriate default value
-            }
+        if(hotel.getFromdate().isBefore(hotel.getTodate())){
+            totalFare = booking(hotel.getNoOfPersons(), hotel.getRates());
         }
-        
         return totalFare;
     }
-    
 
     public double book(Train train) {
         double totalFare = booking(train.getNoOfPersons(),train.getRates());
